26784ab97d51878077e0d162d119bec5043b84e3
Nikunja1310 <nikkisen13@gmail.com>
2023-03-17 00:46:22 +1100

    My Homework week 1 Java- Nikunja

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,26 @@
+package homework_week_5;
+
+/**
+ * Write a program that prints your Bio-Data.
+ */
+public class Programme_1_BioData {
+
+    public static void main(String[] argh) {
+        System.out.println("                          BIO DATA                 ");
+        System.out.println("Name                          :    Nikunja A Senjalia ");
+        System.out.println("Mobile                        :    0470542857          " );
+        System.out.println("Email                         :    niks.sen13@gmail.com     ");
+        System.out.println("Gender                        :    Female");
+        System.out.println("Date of Birth                 :    13-10-1991"  );
+        System.out.println("Marital Status                :    Married "   );
+        System.out.println("Languages Known               :    English, Hindi, Marathi and Gujarati ");
+        System.out.println("Qualification                 :    Software Tester" );
+        System.out.println("Experience                    :    Agile Tester with 5 years of experience    ");
+        System.out.println("Address                       :    15-45 Rosanna road, Heidelberg, 3084");
+        System.out.println("Place                         :    Australia");
+        System.out.println("Date                          :    16/03/2023");
+        System.out.println("Signature                     :    NJinesh");
+
+    }
+
+}
---
+++ src/homework_week_5/Programme_3_PrintInitials.java
@@ -0,0 +1,18 @@
+package homework_week_5;
+/**
+ * 3. Write a program that will print your initials to standard output in letters that
+ * are nine lines tall. Each big letter should be made up of a bunch of *'s.
+ */
+public class Programme_3_PrintInitials {
+    public static void main(String[] args) {
+        System.out.println("**          **     ***************     **               **");
+        System.out.println("** *        **            **            **             **");
+        System.out.println("**   *      **            **             **           **");
+        System.out.println("**    *     **            **              **          ** ");
+        System.out.println("**     *    **            **               **        **" );
+        System.out.println("**      *   **            **                **      **");
+        System.out.println("**       *  **     **     **                 **    **");
+        System.out.println("**        * **      **    **                  **  **  ");
+        System.out.println("**          **       *** **                     **");
+    }
+}
